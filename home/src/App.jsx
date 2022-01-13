e1f20a448131d1f410f99c5f03453e88f2318edd
Lucas Matias Juarez <lujuarez@emeal.nttdata.com>
2022-01-13 02:30:16 -0300

    first lesson

---
+++ home/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.scss";
+import ReactDOM from "react-dom";
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">
+      Home Page Content
+    </div>
+    <Footer />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
