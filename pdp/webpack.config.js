91d8e0f9c81680d6ec38ce269ef6cd09b0a5314c
Lucas Matias Juarez <lujuarez@emeal.nttdata.com>
2022-01-31 23:27:12 -0300

    use v5-react-router-dom for id of products

--- pdp/webpack.config.js
+++ pdp/webpack.config.js
@@ -44,7 +44,8 @@ module.exports = {
       name: "pdp",
       filename: "remoteEntry.js",
       remotes: {
-        home: "home@http://localhost:3000/remoteEntry.js"
+        home: "home@http://localhost:3000/remoteEntry.js",
+        // pdp: "pdp@http://localhost:3001/remoteEntry.js",
       },
       exposes: {},
       shared: {
